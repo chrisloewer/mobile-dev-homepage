19936d0029946dd9a41c17acaa4fc05d96be5b1c
Chris Loewer <chris.loewer@gmail.com>
2016-01-25 22:44:06 -0600

    Page transitions function with keyboard and touch.

--- js/animation-controller.js
+++ js/animation-controller.js
@@ -1,21 +1,74 @@
 
 var currentPage = 1;
-var maxPage = 2;
-
-document.onclick = function () {
-  // document.getElementById('pg_1').className += ' anim-scale-down'
-  var pgOne = document.getElementById('pg_1');
-  //addClass(pgOne, 'anim-scale-down');
-  //toggleClass(pgOne, 'anim-scale-down');
-  //hidePage(pgOne);
-
-  nextPage();
+var maxPage = null;
+touches = {
+  "touchstart": {"x":-1, "y":-1},
+  "touchmove" : {"x":-1, "y":-1},
+  "touchend"  : false,
+  "direction" : "undetermined"
 };
+
+window.onload = function() {
+  maxPage = document.getElementsByClassName('splash-fullscreen').length;
+
+  // Add other Event Listeners
+  window.addEventListener('keyup', keyHandler, false);
+  document.addEventListener('touchstart', touchHandler, false);
+  document.addEventListener('touchmove', touchHandler, false);
+  document.addEventListener('touchend', touchHandler, false);
+};
+
+
+// --------------------------------------- EVENT HANDLERS ------------------------------------ //
+
+function keyHandler(e) {
+
+  switch(e.keyCode) {
+    case 37:  // Left Key
+      prevPage();
+      break;
+    case 39:  // Right Key
+      nextPage();
+      break;
+  }
+}
+
+function touchHandler(e) {
+  var touch;
+  if (typeof e !== 'undefined'){
+    // e.preventDefault();
+    if (typeof e.touches !== 'undefined') {
+      touch = e.touches[0];
+      switch (e.type) {
+        case 'touchstart':
+        case 'touchmove':
+          touches[e.type].x = touch.pageX;
+          touches[e.type].y = touch.pageY;
+          break;
+        case 'touchend':
+          touches[e.type] = true;
+          if (touches.touchstart.y > -1 && touches.touchmove.y > -1) {
+            touches.direction = touches.touchstart.x < touches.touchmove.x ? "right" : "left";
+            var swipeDist = Math.abs(touches.touchstart.x - touches.touchmove.x);
+
+            if(touches.direction == 'left' && swipeDist > 100) {
+              nextPage();
+            }
+            else if(touches.direction == 'right' && swipeDist > 100) {
+              prevPage();
+            }
+          }
+        default:
+          break;
+      }
+    }
+  }
+}
 
 // ---------------------------------------- UTILITIES ---------------------------------------- //
 
 function nextPage() {
-  var firstPage = document.getElementById('pg_' + currentPage);
+  var initialPage = document.getElementById('pg_' + currentPage);
   var secondPage = null;
 
   if (currentPage < maxPage){
@@ -23,20 +76,57 @@ function nextPage() {
   }
   else { return false; }
 
-  hidePage(firstPage);
+  hidePage(initialPage);
   addClass(secondPage, 'current');
+  addClass(secondPage, 'anim-move-from-right');
+
+  secondPage.addEventListener('animationend', helper);
+  function helper() {
+    removeClass(this, 'anim-move-from-right');
+    this.removeEventListener('animationend', helper);
+  }
+
+  currentPage++;
+}
+
+
+function prevPage() {
+  var initialPage = document.getElementById('pg_' + currentPage);
+  var secondPage = null;
+
+  if (currentPage > 1){
+    secondPage = document.getElementById('pg_' + (currentPage-1) );
+  }
+  else { return false; }
+
+  hidePage(initialPage);
+  addClass(secondPage, 'top');
+  addClass(secondPage, 'current');
+  addClass(secondPage, 'anim-move-from-left');
+
+  secondPage.addEventListener('animationend', helper);
+  function helper() {
+    removeClass(this, 'top');
+    removeClass(this, 'anim-move-from-left');
+    this.removeEventListener('animationend', helper);
+  }
+
+  currentPage--;
 }
 
 function hidePage(element) {
-  element.addEventListener('animationend', reset);
-
   addClass(element, 'anim-scale-down');
+  element.addEventListener('animationend', reset);
 }
 
 function reset() {
   console.log('reset called');
 
   removeClass(this, 'current');
+  removeClass(this, 'anim-move-from-right');
+  removeClass(this, 'anim-scale-down');
+
+
   this.removeEventListener('animationend', reset);
 }
 
